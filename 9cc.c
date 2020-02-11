d37ea8688373736add6b5d0ec0eda41f104cb545
tkm742 <takami97531@gmail.com>
2020-02-11 12:19:20 +0900

    単項演算子 +, - を追加

--- 9cc.c
+++ 9cc.c
@@ -63,6 +63,7 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 void gen(Node *node);
 
@@ -199,19 +200,29 @@ Node *expr(){
 }
 
 Node *mul(){
-	Node *node = primary();
+	Node *node = unary();
 
 	for(;;){
 		if(consume('*')){
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		}
 		else if(consume('/')){
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		}
 		else{
 			return node;
 		}
 	}
+}
+
+Node *unary(){
+	if(consume('+')){
+		return primary();
+	}
+	if(consume('-')){
+		return new_node(ND_SUB, new_node_num(0), primary());
+	}
+	return primary();
 }
 
 Node *primary(){
