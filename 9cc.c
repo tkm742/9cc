cefa6a663b45f5fa62d3b93ac9fd72629e4d0fae
tkm742 <takami97531@gmail.com>
2020-02-11 12:06:32 +0900

    四則演算+()を扱えるパーサを追加

--- 9cc.c
+++ 9cc.c
@@ -41,21 +41,17 @@ struct Node {
 };
 
 
-//---- global variable ----
+//---- global variables ----
 
 Token *token; // 現在注目しているトークン
 char *user_input; // 入力プログラム
 
 
-//---- prototype ----
+//---- prototypes ----
 
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
-Node *new_node_num(int val);
-Node *expr();
-Node *mul();
-Node *primary();
 void error_at(char *loc, char *fmt, ...);
 void error(char *fmt, ...);
+
 bool consume(char op);
 void expect(char op);
 int expect_number();
@@ -63,65 +59,15 @@ bool at_eof();
 Token *new_token(TokenKind kind, Token *cur, char *str);
 Token *tokenize(char *p);
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *primary();
+void gen(Node *node);
+
 
 //---- functions ----
-
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
-	Node *node = calloc(1, sizeof(Node));
-	node->kind = kind;
-	node->lhs = lhs;
-	node->rhs = rhs;
-	return node;
-}
-
-Node *new_node_num(int val){
-	Node *node = calloc(1, sizeof(Node));
-	node->kind = ND_NUM;
-	node->val = val;
-	return node;
-}
-
-Node *expr(){
-	Node *node = mul();
-
-	for(;;){
-		if(consume('+')){
-			node = new_node(ND_ADD, node, mul());
-		}
-		else if(consume('-')){
-			node = new_node(ND_SUB, node, mul());
-		}
-		else{
-			return node;
-		}
-	}
-}
-
-Node *mul(){
-	Node *node = primary();
-
-	for(;;){
-		if(consume('*')){
-			node = new_node(ND_MUL, node, primary());
-		}
-		else if(consume('/')){
-			node = new_node(ND_DIV, node, primary());
-		}
-		else{
-			return node;
-		}
-	}
-}
-
-Node *primary(){
-	if(consume('(')){
-		Node *node = expr();
-		expect(')');
-		return node;
-	}
-
-	return new_node_num(expect_number());
-}
 
 void error_at(char *loc, char *fmt, ...){
 	va_list ap;
@@ -203,7 +149,7 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		if(*p == '+' ||  *p == '-'){
+		if(strchr("+-*/()", *p)){
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -221,6 +167,93 @@ Token *tokenize(char *p){
 	return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr(){
+	Node *node = mul();
+
+	for(;;){
+		if(consume('+')){
+			node = new_node(ND_ADD, node, mul());
+		}
+		else if(consume('-')){
+			node = new_node(ND_SUB, node, mul());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+Node *mul(){
+	Node *node = primary();
+
+	for(;;){
+		if(consume('*')){
+			node = new_node(ND_MUL, node, primary());
+		}
+		else if(consume('/')){
+			node = new_node(ND_DIV, node, primary());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+Node *primary(){
+	if(consume('(')){
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+	if(node->kind == ND_NUM){
+		printf("	push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("	pop rdi\n");
+	printf("	pop rax\n");
+
+	switch(node->kind){
+	case ND_ADD:
+		printf("	add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("	sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("	imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("	cqo\n");
+		printf("	idiv rdi\n");
+		break;
+	}
+
+	printf("	push rax\n");
+}
 
 
 int main(int argc, char **argv){
@@ -230,31 +263,22 @@ int main(int argc, char **argv){
         return 1;
     }
 
+	// トークナイズして、抽象構文木を生成
 	user_input = argv[1];
-
-	// トークナイズする
 	token = tokenize(argv[1]);
+	Node *node = expr();
 
 	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// 式の最初が数かチェックし、最初のmov命令を出力
-	printf("	mov rax, %d\n", expect_number());
+	// 抽象構文木を下りながらコード生成
+	gen(node);
 
-	// '+<数>'あるいは'-<数>'というトークンｎ並びを消費しつつ
-	// アセンブリを出力
-	while(!at_eof()){
-		if(consume('+')){
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
-    printf("    ret\n");
+	// スタックトップに式全体の値が残っているはずなので
+	// それをraxにロードし返り値とする
+	printf("	pop rax\n");
+	printf("	ret\n");
     return 0;
 }
